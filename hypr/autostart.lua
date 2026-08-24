2fb9edbbc1812bc8544f0da9b9dcdb103c98e2e5
Jacob Feenstra <jacob.d.feenstra@gmail.com>
2026-08-24 01:15:46 -0700

    make all exec's silent

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -8,7 +8,7 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("waybar")
 	hl.exec_cmd("hyprpaper")
 
-	hl.exec_cmd("notion-app", { workspace = "1" })
+	hl.exec_cmd("notion-app", { workspace = "1 silent" })
 
 	hl.exec_cmd("firefox https://www.gmail.com https://news.ycombinator.com", { workspace = "2 silent" })
 
