c2271509f7775d288edb646a690c9a6911dcd494
Jacob Feenstra <jacob.d.feenstra@gmail.com>
2026-08-24 01:04:25 -0700

    revamp autostart completely- daily workflow is now captured!

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -4,9 +4,30 @@
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function()
-	hl.exec_cmd(Terminal)
 	hl.exec_cmd("nm-applet")
-	hl.exec_cmd("waybar & hyprpaper & firefox")
-	hl.exec_cmd("strawberry & nicotine")
-	hl.exec_cmd("pavucontrol")
+	hl.exec_cmd("waybar")
+	hl.exec_cmd("hyprpaper")
+
+	hl.exec_cmd("notion-app", { workspace = "1" })
+
+	hl.exec_cmd("firefox https://www.gmail.com https://news.ycombinator.com", { workspace = "2 silent" })
+
+	hl.exec_cmd(Terminal, { workspace = "3 silent" })
+
+	hl.exec_cmd("discord", { workspace = "8 silent" })
+	hl.exec_cmd("signal-desktop", { workspace = "8 silent" })
+	hl.exec_cmd("slack", { workspace = "8 silent" })
+
+	hl.exec_cmd("pavucontrol", { workspace = "9 silent" })
+	hl.exec_cmd("blueman-manager", { workspace = "9 silent" })
+
+	hl.exec_cmd("strawberry", { workspace = "10 silent" })
+	hl.exec_cmd("nicotine", { workspace = "10 silent" })
+	hl.exec_cmd("kitty -d $SLSK", { workspace = "10 silent" })
+	hl.exec_cmd(
+		"firefox https://www.rateyourmusic.com https://www.discogs.com \z
+			 https://www.last.fm/user/hawkhobo https://bandcamp.com \z
+			 https://www.mixesdb.com/w/Main_Page https://musicbrainz.org/",
+		{ workspace = "10 silent" }
+	)
 end)
